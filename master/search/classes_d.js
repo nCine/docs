869ba73191ffea964c9d4289f6344b2f9ef4ee5b
Angelo Theodorou <encelo@gmail.com>
2021-01-30 18:37:55 +0100

    Initial commit

---
+++ master/search/classes_d.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['nuklearcontext_0',['NuklearContext',['../classncine_1_1_nuklear_context.html',1,'ncine']]],
+  ['nullaudiodevice_1',['NullAudioDevice',['../classncine_1_1_null_audio_device.html',1,'ncine']]],
+  ['nullgfxcapabilities_2',['NullGfxCapabilities',['../classncine_1_1_null_gfx_capabilities.html',1,'ncine']]],
+  ['nullindexer_3',['NullIndexer',['../classncine_1_1_null_indexer.html',1,'ncine']]],
+  ['nulllogger_4',['NullLogger',['../classncine_1_1_null_logger.html',1,'ncine']]],
+  ['nullthreadpool_5',['NullThreadPool',['../classncine_1_1_null_thread_pool.html',1,'ncine']]]
+];
---
+++ master/search/enums_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['depthstencilformat_0',['DepthStencilFormat',['../classncine_1_1_viewport.html#ac00e6160e3bacd3bb9bf4f4746f54f02',1,'ncine::Viewport']]],
+  ['dirtybitpositions_1',['DirtyBitPositions',['../classncine_1_1_scene_node.html#a07b9d35c1ca971cdf4b03654493ae533',1,'ncine::SceneNode']]]
+];
