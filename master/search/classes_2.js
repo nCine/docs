32bc0260463f8d9601edaaf365d0abee00adc64a
Angelo Theodorou <encelo@gmail.com>
2021-01-30 18:37:55 +0100

    Initial commit

---
+++ master/search/classes_2.js
@@ -0,0 +1,15 @@
+var searchData=
+[
+  ['camera_0',['Camera',['../classncine_1_1_camera.html',1,'ncine']]],
+  ['chorusproperties_1',['ChorusProperties',['../structncine_1_1_audio_effect_properties_1_1_chorus_properties.html',1,'ncine::AudioEffectProperties']]],
+  ['color_2',['Color',['../classncine_1_1_color.html',1,'ncine']]],
+  ['coloraffector_3',['ColorAffector',['../classncine_1_1_color_affector.html',1,'ncine']]],
+  ['colorf_4',['Colorf',['../classncine_1_1_colorf.html',1,'ncine']]],
+  ['colorhdr_5',['ColorHdr',['../classncine_1_1_color_hdr.html',1,'ncine']]],
+  ['colorstep_6',['ColorStep',['../structncine_1_1_color_affector_1_1_color_step.html',1,'ncine::ColorAffector']]],
+  ['compressorproperties_7',['CompressorProperties',['../structncine_1_1_audio_effect_properties_1_1_compressor_properties.html',1,'ncine::AudioEffectProperties']]],
+  ['constructhelpers_8',['constructHelpers',['../structnctl_1_1detail_1_1construct_helpers.html',1,'nctl::detail']]],
+  ['constructhelpers_3c_20true_20_3e_9',['constructHelpers&lt; true &gt;',['../structnctl_1_1detail_1_1construct_helpers_3_01true_01_4.html',1,'nctl::detail']]],
+  ['copyhelpers_10',['copyHelpers',['../structnctl_1_1detail_1_1copy_helpers.html',1,'nctl::detail']]],
+  ['copyhelpers_3c_20true_20_3e_11',['copyHelpers&lt; true &gt;',['../structnctl_1_1detail_1_1copy_helpers_3_01true_01_4.html',1,'nctl::detail']]]
+];
