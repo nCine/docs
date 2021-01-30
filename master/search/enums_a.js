d665ec82accf1389e8f6393a2161344cc54b1761
Angelo Theodorou <encelo@gmail.com>
2021-01-30 18:37:55 +0100

    Initial commit

---
+++ master/search/enums_a.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['rendermode_0',['RenderMode',['../classncine_1_1_font.html#a735257cb2d67a869321ca78922bee774',1,'ncine::Font']]],
+  ['rewindmode_1',['RewindMode',['../classncine_1_1_rect_animation.html#a3e3d07baae76c946ae975f7dc9d740d4',1,'ncine::RectAnimation']]]
+];
