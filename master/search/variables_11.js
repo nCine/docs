059145c13292b5d61b44d71bedb235932c91958f
Angelo Theodorou <encelo@gmail.com>
2021-01-30 18:37:55 +0100

    Initial commit

---
+++ master/search/variables_11.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['texrect_5f_0',['texRect_',['../classncine_1_1_base_sprite.html#a534738fc0a176e5184a2ac3e66ff29b5',1,'ncine::BaseSprite']]],
+  ['text_1',['text',['../classncine_1_1_text_input_event.html#a053c6bf572cd457827a009f6d257655a',1,'ncine::TextInputEvent']]],
+  ['texture_5f_2',['texture_',['../classncine_1_1_base_sprite.html#a0d2de67137146ca2c73981910ef29bea',1,'ncine::BaseSprite']]],
+  ['type_5f_3',['type_',['../classncine_1_1_i_file.html#a5350514d0358e9d154f34215c442b931',1,'ncine::IFile::type_'],['../classncine_1_1_object.html#adaa96983790a2e67e1c7dd35ba65d8cb',1,'ncine::Object::type_'],['../classncine_1_1_particle_affector.html#acb9b6ede8af4799f853a2d87269b39f8',1,'ncine::ParticleAffector::type_']]]
+];
---
+++ master/search/variables_7.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['h_0',['h',['../classncine_1_1_rect.html#a8e0a221c4ada7abb45c7348dd2f47ff3',1,'ncine::Rect']]],
+  ['hatid_1',['hatId',['../classncine_1_1_joy_hat_event.html#aa6dc41d7ed6710ea545cf4c9d22f3078',1,'ncine::JoyHatEvent']]],
+  ['hatstate_2',['hatState',['../classncine_1_1_joy_hat_event.html#a6fe70948f6166fb3f746452ec7da25ae',1,'ncine::JoyHatEvent']]],
+  ['height_5f_3',['height_',['../classncine_1_1_drawable_node.html#a724705ba38b58273b978846f7174b422',1,'ncine::DrawableNode::height_'],['../classncine_1_1_i_gfx_device.html#a7f6da8012e4796198abab3ec580f45c2',1,'ncine::IGfxDevice::height_']]]
+];
